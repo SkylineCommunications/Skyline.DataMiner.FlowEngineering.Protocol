a4b5868f0cc684a01b05422f49a5a34b9fa20683
Tom Waterbley <tom.waterbley@skyline.be>
2023-12-08 13:27:01 +0100

    bugfix when the key in DCF dynamic link contains a ';'

--- FlowEngineering.Protocol/DCF/DcfDynamicLink.cs
+++ FlowEngineering.Protocol/DCF/DcfDynamicLink.cs
@@ -16,7 +16,7 @@
 
 		public static bool TryParse(string input, out DcfDynamicLink dynamicLink)
 		{
-			var parts = input.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+			var parts = input.Split(new[] { ';' }, 2, StringSplitOptions.RemoveEmptyEntries);
 			if (parts.Length >= 2 &&
 				Int32.TryParse(parts[0], out var groupId))
 			{
